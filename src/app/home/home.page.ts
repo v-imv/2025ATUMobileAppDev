717793a22f9a464f155ec1b8218d7a9c27544bd6
v-imv <v.iminov@gmail.com>
2025-12-27 15:17:54 +0000

    Added text input and search button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonButton, IonInput, IonItem } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { settingsOutline, heart } from 'ionicons/icons';
 
@@ -7,10 +7,14 @@ import { settingsOutline, heart } from 'ionicons/icons';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
+  imports: [IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonItem, IonInput],
 })
 export class HomePage {
+
   constructor() {
     addIcons({ settingsOutline, heart });
   }
+
+
+  
 }
