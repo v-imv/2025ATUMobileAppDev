95459b1d7208460b286e1c75a04fd5930ebd9dc8
v-imv <v.iminov@gmail.com>
2025-12-27 14:07:14 +0000

    Added ionic icons and buttons to toolbar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { settingsOutline, heart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ settingsOutline, heart });
+  }
 }
