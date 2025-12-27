48a945154f831a498012dc291e8a3d0b004088ff
v-imv <v.iminov@gmail.com>
2025-12-27 22:58:51 +0000

    implemented settings page logic

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRadioGroup, IonRadio } from '@ionic/angular/standalone';
+import { StorageService } from '../services/storage-service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonRadio, IonRadioGroup, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  constructor(private storageService: StorageService) { }
+
+  measurementSettingKey: string = "measurementSetting";
+  measurementSettingValue: string = "";
 
   ngOnInit() {
+    this.determineMeasurementSettingOnInit();
+  }
+
+  async determineMeasurementSettingOnInit(){
+    let previousSetting = await this.storageService.get(this.measurementSettingKey);
+    if(previousSetting){
+      this.measurementSettingValue = JSON.parse(previousSetting);
+    } else {
+      this.measurementSettingValue = "metric"; // default value
+    }
+  }
+
+  async handleChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const selectedValue = JSON.stringify(target.value);
+    await this.storageService.set(this.measurementSettingKey, selectedValue);
   }
 
 }
