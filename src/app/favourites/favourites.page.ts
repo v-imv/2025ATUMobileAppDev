e66ae9cb87886f16790eb75db342f0b6984fb4b2
v-imv <v.iminov@gmail.com>
2025-12-29 18:14:19 +0000

    added favourites page display

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { StorageService } from '../services/storage-service';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonCardContent, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
 })
 export class FavouritesPage implements OnInit {
 
-  constructor() { }
+  constructor(private storageService: StorageService) { }
+
+  favouritesData: any = [];
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.getFavourites();
+  }
+
+  async getFavourites(){
+    let favouritesArray = JSON.parse(await this.storageService.get("favourites"));
+    if(favouritesArray){
+      this.favouritesData = favouritesArray;
+    }else{
+      this.favouritesData = [];
+    }
   }
 
 }
